0a8de268d354072e93ac528cffb651a3dc1f5626
caramelmelmel <melody_leong@mymail.sutd.edu.sg>
2022-05-31 13:23:04 +0800

    add: login signup route

---
+++ controllers/AuthController.js
@@ -0,0 +1,85 @@
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const User = require('../models/User');
+require('dotenv').config();
+
+const JWT_SECRET = process.env.SECRET_KEY;
+console.log(`JWT Secret: ${JWT_SECRET}`);
+const { emailRegex } = require('../helpers/AuthHelpers');
+
+// registration helper function for user
+const Register = async (req, res) => {
+  // get data
+  const { email } = req.body;
+  let { password } = req.body;
+
+  if (email === '' || password === '') {
+    return res
+      .status(400)
+      .send({ message: 'Please fill in an email or a password' });
+  }
+  // use of email regex to ensure valid email format
+  if (!email.match(emailRegex)) {
+    return res
+      .status(400)
+      .send({ message: 'Please insert a valid email format' });
+  }
+  // password salt for 40 rounds prevents length extension attack
+  const salt = await bcrypt.genSalt(20);
+  password = await bcrypt.hash(password, salt);
+
+  // model sets unique so try catch is used to catch errors
+  try {
+    await User.create({
+      email,
+      password,
+    });
+    return res.status(201).send({ message: 'created account successfully' });
+    // since unique entries hence catching an error in async is sufficient
+  } catch (error) {
+    if (error.code === 11000) {
+      return res.status(400).send({ message: 'email already exists' });
+    }
+    throw error;
+  }
+};
+
+const verifyUserLogin = async (email, password) => {
+  try {
+    const user = await User.findOne({ email }).lean();
+    if (!user) {
+      return { status: 'error', error: 'user not found' };
+    }
+    if (await bcrypt.compare(password, user.password)) {
+      console.log('compared successfully');
+      // creating a JWT token
+      const token = jwt.sign(
+        // eslint-disable-next-line no-underscore-dangle
+        { id: user._id, username: user.email, type: 'user' },
+        JWT_SECRET,
+        { expiresIn: '2h' }
+      );
+      return { status: 'ok', data: token };
+    }
+    return { status: 'error', error: 'invalid password' };
+  } catch (error) {
+    console.log(error);
+    return { status: 'error', error: 'timed out' };
+  }
+};
+
+// login helper function for user
+const Login = async (req, res) => {
+  const { email, password } = req.body;
+
+  const response = await verifyUserLogin(email, password);
+
+  if (response.status === 'ok') {
+    res.status(200).send({ data: response.data });
+  } else {
+    res.status(400).send({ message: 'Login unsuccessful' });
+  }
+};
+
+exports.login = Login;
+exports.register = Register;
--- index.js
+++ index.js
@@ -1,14 +1,40 @@
+require('./config/db.config').connection;
+
 // requirements
 const express = require('express');
+const cors = require('cors');
+
+const Auth = require('./controllers/AuthController');
+const QRRoute = require('./Routes/QRRoute');
 
 const app = express();
-const port = 3000;
 
-var bodyParser = require('body-parser');
+// env variables
+require('dotenv').config();
+
+const port = 8000;
+
+// allowed origins of access
+const corsOptions = {
+  origin: 'http://localhost:3000',
+};
+
+app.use(cors(corsOptions));
+
+// parse requests of content-type - application/json and application/x-www-form-urlencoded
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
-  res.send('hello world');
+  res.status(200).send('Welcome to getSearching');
 });
+
+app.post('/register', Auth.register);
+
+app.post('/login', Auth.login);
+
+// qr code related routes
+app.use('/qrcode', QRRoute.Router);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
